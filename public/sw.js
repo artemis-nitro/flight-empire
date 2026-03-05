39751ee9c6f18405c060e4c4731b75aead5e23e2
artemis-nitro <artemisnitrox42@gmail.com>
2026-03-05 18:01:26 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE = "skytycoon-v1";
+const ASSETS = ["/", "/index.html", "/assets/"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(["/", "/index.html"]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
